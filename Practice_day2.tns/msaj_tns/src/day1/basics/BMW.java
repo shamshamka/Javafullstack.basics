e3162deb75241f6421a857e014078c95b02b94c1
shamshamka <shamshamka19@gmail.com>
2025-08-08 13:48:47 +0530

    javabasics2

---
+++ Practice_day2.tns/msaj_tns/src/day1/basics/BMW.java
@@ -0,0 +1,17 @@
+package day1.basics;
+
+public class BMW {
+	
+	void show() {
+		System.out.println("Hii");
+	}
+	
+	
+	public static void main(String[] args) {
+		BMW obj = new BMW();
+		obj.show();
+	}
+
+}
+
+ 
